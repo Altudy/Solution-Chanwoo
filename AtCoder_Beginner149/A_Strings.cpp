ae474fe619fd1c51773f3a996ccd08b409e974c9
ChanWoo25 <50766175+ChanWoo25@users.noreply.github.com>
2019-12-29 22:10:22 +0900

    Create A_Strings.cpp

---
+++ AtCoder_Beginner149/A_Strings.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <deque>
+ 
+#define Inf             987654321
+using namespace std;
+ 
+typedef long long int   ll;
+typedef pair<ll, ll>    pii;
+typedef vector<ll>      vi;
+typedef vector<vi>      vii;
+/*************************************************/
+ 
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(); cout.tie();
+    string t, s;
+    cin >> s >> t;
+    cout << t + s << "\n";
+    return 0;
+}
