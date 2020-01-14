7ecc6a37621e575fd6f8cb23cd31a27f017f5902
ChanWoo25 <50766175+ChanWoo25@users.noreply.github.com>
2020-01-14 18:14:56 +0900

    #lev1

--- AtCoder_Beginner151/B_Achieve the Goal.cpp
+++ AtCoder_Beginner151/B_Achieve the Goal.cpp
@@ -1,1 +1,23 @@
-
+//#lev1
+#include <iostream>
+using namespace std;
+ 
+int N, K, M;
+int A[101];
+int main() {
+    cin >> N >> K >> M;
+    int sum = 0;
+    for (int i = 1; i < N; i++) {
+        cin >> A[i];
+        sum += A[i];
+    }
+    int want = N * M;
+    if (want - sum <= 0)
+        cout << 0;
+    else if (want - sum <= K)
+        cout << want - sum;
+    else
+        cout << -1;
+ 
+    return 0;
+}
