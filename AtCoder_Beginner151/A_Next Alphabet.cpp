84873efdb0f581e5cc9f946532e5058f5a7ece17
ChanWoo25 <50766175+ChanWoo25@users.noreply.github.com>
2020-01-14 18:14:37 +0900

    #lev1

--- AtCoder_Beginner151/A_Next Alphabet.cpp
+++ AtCoder_Beginner151/A_Next Alphabet.cpp
@@ -1,5 +1,3 @@
-// A
-
 #include <iostream>
 using namespace std;
  
@@ -11,27 +9,3 @@ int main() {
     return 0;
 }
 
-// B
-
-#include <iostream>
-using namespace std;
- 
-int N, K, M;
-int A[101];
-int main() {
-    cin >> N >> K >> M;
-    int sum = 0;
-    for (int i = 1; i < N; i++) {
-        cin >> A[i];
-        sum += A[i];
-    }
-    int want = N * M;
-    if (want - sum <= 0)
-        cout << 0;
-    else if (want - sum <= K)
-        cout << want - sum;
-    else
-        cout << -1;
- 
-    return 0;
-}
