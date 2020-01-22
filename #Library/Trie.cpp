ba5b779108aacebe302853fcc514204192384a4d
ChanWoo25 <50766175+ChanWoo25@users.noreply.github.com>
2020-01-22 20:38:58 +0900

    Update Trie.cpp

--- #Library/Trie.cpp
+++ #Library/Trie.cpp
@@ -1,18 +1,19 @@
 /**********TRIE*********TRIE*********TRIE********TRIE*****/
 
-//다루고 싶은 문자 종류에 따라 변경하여 사용.
+//change to suit your needs
 const int BRANCH_SIZE = 26;
-int toNumber(char ch) { return ch - 'A'; }
+int toNumber(const char& ch) { return ch - 'A'; }
 
 struct TrieNode {
+public:
     TrieNode* children[BRANCH_SIZE];
     bool isEndOfWord;
 
     // Constructor
     TrieNode() : isEndOfWord(false) {
-        // 다음 TrieNode를 가리키는 children 초기값은 
-        // 모두 NULL로 비어있음을 보장하도록 합니다.
-        memset(children, NULL, sizeof(children));
+        // Initialize all children points to null;
+        for (int i = 0; i < BRANCH_SIZE; i++)
+            children[i] = 0;
     }
     // Destructor
     ~TrieNode() {
@@ -28,11 +29,11 @@ struct TrieNode {
         // INSERT OPERATION 
         for (int i = 0; i < len; i++) {
             int next = toNumber(key[i]);
-            if (children[next] == NULL) //자식 노드가 없다면 생성.
-                children[next] = new TrieNode();
+            if (here->children[next] == NULL) //자식 노드가 없다면 생성.
+                here->children[next] = new TrieNode();
             here = here->children[next]; //다음 문자로 재귀 호출.
         }
-        isEndOfWord = true;
+        here->isEndOfWord = true;
     }
 
     bool find(const string& key) {
@@ -41,13 +42,12 @@ struct TrieNode {
         if (len == 0) return false;         // Choose for empty strings.
 
         for (int i = 0; i < len; i++) {     //If isEndOfWord == true, return here.
-            int next = toNumber(key[i]);    //If not, search next character.
-            if (here->children[i] == NULL)
+            int next = toNumber(key[next]);    //If not, search next character.
+            if (here->children[next] == NULL)
                 return false;
-            here = here->children[i];
+            here = here->children[next];
         }
-
-        return here->isEndOfWord;     // Recursive
+        return here->isEndOfWord;
     }
 
 };
