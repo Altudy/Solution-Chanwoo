796e30a0b3ff94ce93813820f753830f0dbc742f
ChanWoo25 <50766175+ChanWoo25@users.noreply.github.com>
2020-02-10 23:52:11 +0900

    Create FORM.cpp

---
+++ #Library/FORM.cpp
@@ -0,0 +1,52 @@
+/* #                          */
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <string>
+#include <deque>
+#include <map>
+#include <iomanip>
+#include <cassert>
+#include <cstdio>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+//#pragma warning(disable:4996)
+
+using namespace std;
+
+
+/** Type Define **/
+typedef long long int           ll;
+typedef pair<int, int>          pii;
+typedef pair<ll, ll>            pll;
+typedef vector<string>          vs;
+typedef vector<bool>            vb;
+typedef vector<int>             vi;
+typedef vector<long long>		vl;
+typedef vector<vi>              vvi;
+typedef vector<vl>				vvl;
+typedef vector<char>            vch;
+
+#define all(x)  x.begin(), x.end()
+#define pb(x)   push_back(x)
+constexpr int           inf = 0x3f3f3f3f;
+constexpr long long 	INF = 9123456789123456789;
+constexpr long long     MOD = 1000000007LL;
+/*************************************************/
+int N;
+
+void sol() {
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(12);
+    cin.tie(nullptr);
+
+
+    return 0;
+}
