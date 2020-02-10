9e3ebdb92b7d4788ab947379706947fd3f43d766
ChanWoo25 <50766175+ChanWoo25@users.noreply.github.com>
2020-02-10 17:34:40 +0900

    #lev1

---
+++ AtCoder_Beginner154/B_I miss you.cpp
@@ -0,0 +1,52 @@
+/* #lev1                          */
+ 
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <string>
+#include <deque>
+#include <map>
+//#pragma warning(disable:4996)
+ 
+using namespace std;
+ 
+ 
+/** Type Define **/
+typedef long long int           ll;
+typedef pair<int, int>          pii;
+typedef pair<ll, ll>            pll;
+typedef vector<string>          vs;
+typedef vector<bool>            vb;
+typedef vector<int>             vi;
+typedef vector<long long>		vl;
+typedef vector<vi>              vvi;
+typedef vector<vl>				vvl;
+typedef vector<char>            vch;
+ 
+constexpr int               inf =   	0x3f3f3f3f;
+constexpr long long 		INF = 		9123456789123456789;
+constexpr long long         MOD =       1000000007LL;
+/*************************************************/
+string s;
+ 
+void sol() {
+ 
+}
+ 
+int main() {
+	ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(10);
+    cin.tie(nullptr);
+	cin >> s;
+  	for(int i=0; i<s.size(); i++)
+      cout << 'x';
+  	cout <<endl;
+ 
+    return 0;
+}
