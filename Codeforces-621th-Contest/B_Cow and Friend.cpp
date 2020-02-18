82e0b0b5cf0e61229232e47b7d8ba916371a2e0f
Chanwoo Lee <50766175+ChanWoo25@users.noreply.github.com>
2020-02-18 18:36:29 +0900

    #lev2 #그리디

--- Codeforces-621th-Contest/B_Cow and Friend.cpp
+++ Codeforces-621th-Contest/B_Cow and Friend.cpp
@@ -1,5 +1,5 @@
-/* #lev2 #그리디                          */
- 
+/* #lev2 #그리디   */
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -14,10 +14,10 @@
 #include <cstdint>
 #include <cstring>
 //#pragma warning(disable:4996)
- 
+
 using namespace std;
- 
- 
+
+
 /** Type Define **/
 typedef long long int           ll;
 typedef pair<int, int>          pii;
@@ -29,23 +29,23 @@ typedef vector<long long>		vl;
 typedef vector<vi>              vvi;
 typedef vector<vl>				vvl;
 typedef vector<char>            vch;
- 
+
 #define all(x)  x.begin(), x.end()
 #define pb(x)   push_back(x)
 constexpr int           inf = 0x3f3f3f3f;
 constexpr long long 	INF = 9123456789123456789;
 constexpr long long     MOD = 1000000007LL;
 /*************************************************/
- 
+
 void sol() {
     ll n, x;
     cin >> n >> x;
     vl a(n);
     for (auto& w : a) cin >> w;
- 
+
     sort(a.rbegin(), a.rend());
     if (x % a[0] == 0) { cout << (x / a[0]) << '\n'; return; }
- 
+
     if (x < a[0]) {
         for (int i = 1; i < n; i++)
             if (x == a[i]) {
@@ -62,16 +62,40 @@ void sol() {
         }
         cout << min(c1, c2) << '\n';
     }
- 
+
 }
- 
+
+void MX(ll& a, ll& b) {
+    if (a < b) a = b;
+}
+void sol2() {
+    ll n, x, e, mx(-1);
+    cin >> n >> x;
+    bool chk(false);
+
+    while (n--) {
+        cin >> e;
+        if (e == x) chk = true;
+        MX(mx, e);
+    }
+
+    if (chk) { cout << "1\n"; return; }
+    
+    //x가 mx로 나누어 떨어질 때와 나누어 떨어지지 않을 때를 동시에 커버가능.
+    // 만약 나누어 떨어진다면 -1을 통해 mx를 더하여도 1이 증가하지 않게 해줄것이고
+    // 나누어 떨어지지 않는다면, -1을 한다고 하더라도 x/mx + 1 과 답이 같을 것이다.
+    cout << max(2LL, (x + mx - 1) / mx) << '\n';
+    
+
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cout << fixed << setprecision(12);
     cin.tie(nullptr);
     int T; cin >> T;
     while (T--)
-        sol();
- 
+        sol2();
+
     return 0;
 }
