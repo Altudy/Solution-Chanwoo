7a33bbf7a909959d1901cc5ea4f52439274ba396
Chanwoo Lee <50766175+ChanWoo25@users.noreply.github.com>
2020-03-03 19:25:16 +0900

    #lev1 #허무하네 #BF

--- AtCoder_Beginner156/C_Rally.cpp
+++ AtCoder_Beginner156/C_Rally.cpp
@@ -1,34 +1,64 @@
 // #lev1 #허무하네 #BF
 
+/* #                          */
+
 #include <iostream>
-#include <string>
-#include <vector>
 #include <algorithm>
-#include <set>
+#include <vector>
+#include <queue>
+#include <string>
+#include <deque>
 #include <map>
+#include <iomanip>
+#include <cassert>
+#include <cstdio>
 #include <cmath>
+#include <cstdint>
+#include <cstring>
+//#pragma warning(disable:4996)
 
 using namespace std;
-using LL = long long;
 
+
+/** Type Define **/
+typedef long long int           ll;
+typedef pair<int, int>          pii;
+typedef pair<ll, ll>            pll;
+typedef vector<string>          vs;
+typedef vector<bool>            vb;
+typedef vector<int>             vi;
+typedef vector<long long>       vl;
+typedef vector<vi>              vvi;
+typedef vector<vl>              vvl;
+typedef vector<char>            vch;
+#define     it(x)  x.begin() 
+#define     all(x)  x.begin(), x.end()
+#define     pb(x)   push_back(x)
+constexpr int           inf = 0x3f3f3f3f;
+constexpr long long     INF = 9123456789123456789;
+constexpr long long     MOD = 1000000007LL;
+/*************************************************/
+
+int n;
+int xi;
+
+void MN(int& x, const int& y){
+    if(x > y) x = y;
+}
 int main(){
-    int n;
-    int xi;
     cin >> n;
-    vector<int> x;
-    for(int i=0; i<n; i++){
-        cin >> xi;
-        x.push_back(xi);
-    }
+    vector<int> X(n);
+    for(auto& x : X) cin >> x;
 
     int sum;
-    int ans = 10000000;
-    for(int p=1; p<101; p++){
+    int ans = inf;
+
+    for(int p=1; p<101; p++){ // Check every staring point. -> Brute Force
         sum = 0;
         for(int i=0; i<n; i++){
             sum += (x[i]-p)*(x[i]-p);
         }
-        ans = min(ans, sum);
+        MN(ans, sum);
     }
 
     cout << ans << endl;
