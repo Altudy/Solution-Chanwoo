8409434c5d5a7ebfb786b171cb9048bee8e82535
Chanwoo Lee <50766175+ChanWoo25@users.noreply.github.com>
2020-03-29 02:38:39 +0900

    /* #lev5 #Tree #DFS #Math */

--- AtCoder_Beginner160/F_Distributing Integers.cpp
+++ AtCoder_Beginner160/F_Distributing Integers.cpp
@@ -1,4 +1,10 @@
 /* #lev5 #Tree #DFS #Math */
+/*
+ - 주어진 Edge들로 트리 DFS를 구현하기 위해 인접한 노드들을 linked list로 나타낸 뒤,
+ - 각 노드에서 부모노드를 제외한 나머지를 자식노드로 하여 DFS(child_node)를 진행.
+ - 노드 1을 기준으로 해서 각각 자식들을 루트로 변형했을 때, Group 변수를 어떻게 조정할지 수학적으로 떠올릴 수 있어야함.
+ - Multiplicative Inverse를 구현할 줄 알아야함.
+*/
 
 #include <iostream>
 #include <algorithm>
@@ -40,7 +46,7 @@ constexpr long long     MOD = 1000000007LL;
 
 const int MAXN = 200010, P = 1e9 + 7;
 int n, x, y, _edge, _head[MAXN], _vertex[2 * MAXN], _next[2 * MAXN], _size[MAXN];
-int f[MAXN]; // f[i] i를 루트로 트리를 만들었을 때, 순서가 강제되는만큼 반영. ans[i] = n! % f[i] 이 된다.
+int group[MAXN]; // f[i] i를 루트로 트리를 만들었을 때, 순서가 강제되는만큼 반영. ans[i] = n! % f[i] 이 된다.
 
 void add_edge(int x, int y) {
     _vertex[++_edge] = y;
@@ -68,7 +74,7 @@ void dfs1(int x, int parent) {
         dfs1(_vertex[e], x);
         _size[x] += _size[_vertex[e]];
     }
-    f[1] = (1ll * f[1] * _size[x]) % P;
+    group[1] = (1ll * group[1] * _size[x]) % P;
     return;
 }
 
@@ -77,8 +83,8 @@ void dfs2(int x, int parent) {
         int y = _vertex[e];
 
         if (y == parent) continue;
-        ll temp = 1ll * (n - _size[y]) * mul_inv_of(_size[y]) % P;
-        f[y] = ( 1ll * f[x] * temp ) % P;
+        ll temp = 1ll * ((ll)n - _size[y]) * mul_inv_of(_size[y]) % P;
+        group[y] = ( 1ll * group[x] * temp ) % P;
         dfs2(y, x);
     }
     return;
@@ -90,7 +96,7 @@ int main() {
     cin.tie(nullptr);
     cin >> n;
 
-    f[1] = 1;
+    group[1] = 1;
     for (int i = 1; i <= n - 1; i++) {
         cin >> x >> y;
         add_edge(x, y), add_edge(y, x);
@@ -104,7 +110,7 @@ int main() {
         tmp = (1ll * tmp * i) % P; // n! % mod
 
     for (int i = 1; i <= n; i++) 
-        cout << (1ll * tmp * mul_inv_of(f[i]) ) % P 
+        cout << (1ll * tmp * mul_inv_of(group[i]) ) % P
              << '\n';
 
     return 0;
