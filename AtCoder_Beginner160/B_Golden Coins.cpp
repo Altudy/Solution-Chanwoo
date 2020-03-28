01fd25b983f137499e225f26dcf95df441a7f623
Chanwoo Lee <50766175+ChanWoo25@users.noreply.github.com>
2020-03-29 02:49:11 +0900

    #lev1

---
+++ AtCoder_Beginner160/B_Golden Coins.cpp
@@ -0,0 +1,58 @@
+/* #                          */
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <string>
+#include <deque>
+#include <map>
+#include <iomanip>
+#include <functional>
+#include <cassert>
+#include <cstdio>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+//#pragma warning(disable:4996)
+
+using namespace std;
+
+
+/** Type Define **/
+typedef long long int           ll;
+typedef pair<int, int>          pii;
+typedef pair<ll, ll>            pll;
+typedef vector<string>          vs;
+typedef vector<bool>            vb;
+typedef vector<int>             vi;
+typedef vector<long long>		vl;
+typedef vector<vi>              vvi;
+typedef vector<vl>				vvl;
+typedef vector<char>            vch;
+#define		it(x)  x.begin() 
+#define		all(x)  x.begin(), x.end()
+#define		pb(x)   push_back(x)
+constexpr int           inf = 0x3f3f3f3f;
+constexpr long long 	INF = 9123456789123456789;
+constexpr long long     MOD = 1000000007LL;
+/*************************************************/
+void sol() {
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(12);
+    cin.tie(nullptr);
+
+    int X; cin >> X;
+
+    int c500 = X / 500; 
+    int rem = X % 500;
+    int c50 = rem / 5;
+    
+    cout << 1000 * c500 + 5 * c50 << '\n';
+
+    return 0;
+}
