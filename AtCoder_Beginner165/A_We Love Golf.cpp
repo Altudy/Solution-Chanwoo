6d768493fd0bf86d94bd72835878ea6ce8f56046
Chanwoo Lee <50766175+ChanWoo25@users.noreply.github.com>
2020-05-03 23:49:06 +0900

    #lev1

---
+++ AtCoder_Beginner165/A_We Love Golf.cpp
@@ -0,0 +1,67 @@
+/* #lev1 */
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <string>
+#include <deque>
+#include <map>
+#include <iomanip>
+#include <functional>
+#include <cassert>
+#include <cstdio>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+//#pragma warning(disable:4996)
+
+using namespace std;
+
+
+/** Type Define **/
+typedef long long int           ll;
+typedef pair<int, int>          pii;
+typedef pair<ll, ll>            pll;
+typedef vector<string>          vs;
+typedef vector<bool>            vb;
+typedef vector<int>             vi;
+typedef vector<long long>		vl;
+typedef vector<vi>              vvi;
+typedef vector<vl>				vvl;
+typedef vector<char>            vch;
+#define		it(x)  x.begin() 
+#define		all(x)  x.begin(), x.end()
+#define		pb(x)   push_back(x)
+constexpr int           inf = 0x3f3f3f3f;
+constexpr long long 	INF = 9123456789123456789;
+constexpr long long     MOD = 1000000007LL;
+/*************************************************/
+void sol() {
+    int k, a, b;
+    cin >> k >> a >> b;
+
+    int chk = 0;
+    while (chk <= b) {
+        if (chk >= a && chk <= b) {
+            cout << "OK\n";
+            return;
+        }
+        
+        chk += k;
+    }
+
+    cout << "NG\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(12);
+    cin.tie(nullptr);
+    int T(1);
+    while (T--)
+        sol();
+
+
+    return 0;
+}
